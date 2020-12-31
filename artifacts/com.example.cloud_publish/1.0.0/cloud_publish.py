4463321e169fafe12fd6895890d9b23b311b072a
Gavin Adams <me@gavinadams.org>
2020-12-31 09:26:41 -0800

    testing commit, rollback of VM, etc.

---
+++ artifacts/com.example.cloud_publish/1.0.0/cloud_publish.py
@@ -0,0 +1,61 @@
+import os
+import time
+
+import awsiot.greengrasscoreipc.client as client
+from awscrt.io import (
+    ClientBootstrap,
+    DefaultHostResolver,
+    EventLoopGroup,
+    SocketDomain,
+    SocketOptions,
+)
+from awsiot.eventstreamrpc import Connection, LifecycleHandler, MessageAmendment
+from awsiot.greengrasscoreipc.model import PublishToIoTCoreRequest, QOS
+
+TIMEOUT = 10
+
+
+class IPCUtils:
+    def connect(self):
+        elg = EventLoopGroup()
+        resolver = DefaultHostResolver(elg)
+        bootstrap = ClientBootstrap(elg, resolver)
+        socket_options = SocketOptions()
+        socket_options.domain = SocketDomain.Local
+        amender = MessageAmendment.create_static_authtoken_amender(os.getenv("SVCUID"))
+        hostname = os.getenv("AWS_GG_NUCLEUS_DOMAIN_SOCKET_FILEPATH_FOR_COMPONENT")
+        connection = Connection(
+            host_name=hostname,
+            port=8033,
+            bootstrap=bootstrap,
+            socket_options=socket_options,
+            connect_message_amender=amender,
+        )
+        self.lifecycle_handler = LifecycleHandler()
+        connect_future = connection.connect(self.lifecycle_handler)
+        connect_future.result(TIMEOUT)
+        return connection
+
+
+ipc_utils = IPCUtils()
+connection = ipc_utils.connect()
+ipc_client = client.GreengrassCoreIPCClient(connection)
+
+counter = 1
+
+while True:
+    topic = os.environ["CLOUD_TOPIC"]
+    message = f"Hello, World, for the past {counter} times."
+    counter += 1
+    qos = QOS.AT_LEAST_ONCE
+
+    request = PublishToIoTCoreRequest()
+    request.topic_name = topic
+    request.payload = bytes(message, "utf-8")
+    request.qos = qos
+    operation = ipc_client.new_publish_to_iot_core()
+    operation.activate(request)
+    future = operation.get_response()
+    future.result(TIMEOUT)
+
+    time.sleep(5)
